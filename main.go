f67b147528de462dbf5cab7dbb3a5c50c5947091
like <dev@gmail.com>
2019-06-21 16:38:51 +0400

    update

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"collyScrapy/logic"
 	"collyScrapy/models"
 	_ "collyScrapy/routers"
 	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
-	"net/url"
 )
 
 func init() {
@@ -16,7 +16,7 @@ func init() {
 	db_password := beego.AppConfig.String("db_password")
 	db_port := beego.AppConfig.String("db_port")
 	db_name := beego.AppConfig.String("db_name")
-	mysqlConnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&loc=%s", db_user, db_password, db_host, db_port, db_name, url.QueryEscape("Asia/shanghai"))
+	mysqlConnection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", db_user, db_password, db_host, db_port, db_name)
 	//注册驱动
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	//设置默认数据库
@@ -32,11 +32,11 @@ func main() {
 	//}
 	//go logic.StartDownloadVideo()
 
-	//go logic.StartTrans()
-	var video models.Video
-	video.Id = 1
-	video.VideoDir = "100"
-	video.Num = "1111111"
-	video.SetVideoStatus(models.VideoWaitTrans, "video_dir", "num")
+	go logic.StartTrans()
+	//var video models.Video
+	//video.Id = 1
+	//video.VideoDir = "100"
+	//video.Num = "1111111"
+	//video.SetVideoStatus(models.VideoWaitTrans, "video_dir", "num")
 	beego.Run()
 }
